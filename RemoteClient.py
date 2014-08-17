50b62d75be2efa6791d202e7bdb7027326567aca
nihlaeth <info@nihlaeth.nl>
2014-08-17 15:18:45 +0200

    Corrected port

--- RemoteClient.py
+++ RemoteClient.py
@@ -6,6 +6,6 @@ import socket
 import sys
 message = sys.argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 5757)) #seshata
+s.connect(("127.0.0.1", 64502))
 s.send(message)
 s.close()
